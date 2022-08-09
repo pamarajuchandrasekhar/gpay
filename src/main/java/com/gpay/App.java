2290be024dd88217acd7610b92e57a2b21760ef5
chandu <chandu@gmail.com>
2022-08-09 07:52:45 +0000

    gpay

---
+++ src/main/java/com/gpay/App.java
@@ -0,0 +1,13 @@
+package com.gpay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
